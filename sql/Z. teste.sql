1c619954b149518eef49197c037d92d868fdfb38
Almeida <williamgabrielg@gmail.com>
2022-03-17 18:44:04 -0300

    modelos

--- sql/Z. teste.sql
+++ sql/Z. teste.sql
@@ -1,7 +1,7 @@
 --##### DADOS PARA TESTE #####--
 
--- INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (1, 'DF', '70688-290', 'Brasília', 'Setor Noroeste', 'Quadra CRNW 511 Bloco A');
--- INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (2, 'DF', '71742-002', 'Brasília', 'Asa Norte', 'Quadra EQN 412/413');
+INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (1, 'DF', '70688-290', 'Brasília', 'Setor Noroeste', 'Quadra CRNW 511 Bloco A');
+INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (2, 'DF', '71742-002', 'Brasília', 'Asa Norte', 'Quadra EQN 412/413');
 -- INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (3, 'DF', '72548-241', 'Brasília', 'Santa Maria', 'Quadra CL 418 Bloco A');
 -- INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (4, 'DF', '71805-719', 'Brasília', 'Riacho Fundo I', 'Quadra QN 7 Conjunto 19');
 -- INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (5, 'DF', '71994-145', 'Brasília', 'Setor Habitacional Arniqueira', 'Conjunto SHA Conjunto 4 Chácara 23');
@@ -27,10 +27,10 @@
 INSERT INTO CATEGORIA(CategoriaId, Categoria, Lugares, MalaPequena, MalaGrande, Carga, TipoDirecao, CambioAT, Extra) VALUES
 	-- A Comum
 	('A0', 'Compacto', '5', 1, 1, null, null, 0, ''),
-	('AE', 'Econômico', '5', 1, 2, null, 'Hidráulica', 0, ''),
-	('AEx', 'Econômico Sedan', '5', 2, 2, null, 'Hidráulica', 0, ''),
-	('AS', 'Intermediário', '5', 2, 2, null, 'Hidráulica', 0, 'Motor 1.6'),
-	('ASt', 'Intermediário AT', '5', 2, 2, null, 'Hidráulica', 1, 'Motor 1.6'),
+	('AS', 'Econômico', '5', 1, 2, null, 'Hidráulica', 0, ''),
+	('ASx', 'Econômico Sedan', '5', 2, 2, null, 'Hidráulica', 0, ''),
+	('AX', 'Intermediário', '5', 2, 2, null, 'Hidráulica', 0, 'Motor 1.6'),
+	('AXt', 'Intermediário AT', '5', 2, 2, null, 'Hidráulica', 1, 'Motor 1.6'),
 	-- B Adaptado
 	('B', 'Adaptado', '5', 2, 5, null, 'Hidráulica', 1, ''),
 	-- G Grande
@@ -53,11 +53,36 @@ INSERT INTO CATEGORIA(CategoriaId, Categoria, Lugares, MalaPequena, MalaGrande, 
 	('SS', 'Suv Especial', '5', 2, 3, null, 'Hidráulica', 1, ''),
 	('SX', 'Suv Elite', '7', 2, 3, null, 'Elétrica', 1, 'Banco de Couro,Diesel');
 	-- Y Esportivo?
-
 -----------
-INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES (1, 'Uno Drive 1.0', 'Fiat', null, 'Manual', 2020, 49184.00, 1);
-INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES (2, 'Mobi 1.0', 'Fiat', 'Hidráulica', 'Manual', 2018, 46656, 1);
-INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES (3, 'Sandero 1.0', 'Renault', 'Hidráulica', 'Manual', 2020, 53195, 2);
+INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES
+	(1, 'Uno 1.0 Drive', 'Fiat', '', 'Manual', 2021, 54830.00, 'A0'),
+	(2, 'Mobi 1.0 Easy', 'Fiat', '', 'Manual', 2022, 49301.00, 'A0'),
+	(3, 'Kwid 1.0 Zen', 'Renault', '', 'Manual', 2022, 56189.00, 'A0'),
+	(4, 'Gol 1.0', 'Volkswagen', '', 'Manual', 2022, 65090.00, 'A0');
+INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES
+	(5, 'Sandero Zen', 'Renault', 'Hidráulica', 'Manual', 2022, 69671.00, 'AS'),
+	(6, 'Argo 1.0', 'Fiat', 'Hidráulica', 'Manual', 2022, 69765.00, 'AS'),
+	(7, 'Polo 1.0', 'Volkswagen', 'Hidráulica', 'Manual', 2021, 73735.00, 'AS'),
+	(8, '208 1.6 Like', 'Peugeot', 'Hidráulica', 'Manual', 2022, 73796.00, 'AS'),
+	(9, 'HB20 1.0 Evolution', 'Peugeot', 'Hidráulica', 'Manual', 2022, 74407.00, 'AS'),
+	(10, 'Onix 1.0 LT', 'Chevrolet', 'Hidráulica', 'Manual', 2022, 74812.00, 'AS');
+INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES 
+	(11, 'Voyage 1.0', 'Volkswagen', 'Hidráulica', 'Manual', 2022, 70960.00, 'ASx'),
+	(12, 'Logan Zen', 'Renault', 'Hidráulica', 'Manual', 2022, 71469.00, 'ASx'),
+	(13, 'Prisma 1.4 LTZ', 'Chevrolet', 'Hidráulica', 'Manual', 2019, 71553.00, 'ASx'),
+	(14, 'Versa 1.6 Sense', 'Nissan', 'Hidráulica', 'Manual', 2021, 72990.00, 'ASx'),
+	(15, 'HB20S 1.6 Vision', 'Hyundai', 'Hidráulica', 'Manual', 2022, 78213.00, 'ASx');
+INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES 
+	(16, 'Cobalt LT 1.4 8V (Flex)', 'Chevrolet', 'Hidráulica', 'Manual', 2021, 67269.00, 'AX'),
+	(17, 'Etios Sedan X Plus 1.5 (Flex)', 'Toyota', 'Hidráulica', 'Manual', 2021, 69439.00, 'AX'),
+	(18, 'Virtus 1.6 MSI (Flex)', 'Volkswagen', 'Hidráulica', 'Manual', 2020, 76071.00, 'AX');
+INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES 
+	(19, 'Etios Sedan X Plus 1.5 (Flex) (Aut)', 'Toyota', 'Hidráulica', 'Automático', 2021, 79716.00, 'AXt'),
+	(20, 'Virtus 1.6 MSI (Flex) (Aut)', 'Volkswagen', 'Hidráulica', 'Automático', 2020, 81650.00, 'AXt'),
+	(21, 'HB20S 1.6 Vision (Aut)', 'Chevrolet', 'Hidráulica', 'Automático', 2022, 82097.00, 'AXt');
+INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoModelo, CategoriaId) VALUES
+	(22, 'Ecosport 1.5 SE', 'Ford', 'Hidráulica', 'Manual', 2021, 80069.00, 'S0'),
+	(23, 'Ecosport 2.0 Storm 4WD (Aut)', 'Ford', 'Hidráulica', 'Automático', 2021, 106485.00, 'S0t');
 -----------
 INSERT INTO LOCADORA(LocadoraId, Locadora, EnderecoId) VALUES (1, 'Dopler Noroeste', 1);
 INSERT INTO LOCADORA(LocadoraId, Locadora, EnderecoId) VALUES (2, 'Dopler Asa Norte', 2);
@@ -113,7 +138,6 @@ CREATE PROCEDURE CadastraCliente(xNome varchar(40), xClienteId varchar(14), xRg 
 DELIMITER ;
 DROP PROCEDURE CadastraCliente;
 
-
 -- Registra PAGAMENTO
 DELIMITER //
 CREATE PROCEDURE ValorTotalAluguel(xLocacaoId int, xClienteId varchar(14), xGaragemId int)
@@ -138,7 +162,7 @@ DROP PROCEDURE ValorTotalAluguel;
 --##### /PROCEDIMENTOS/ #####--
 
 --##### GATILHOS #####--
-
+SHOW Triggers;
 -- Registra data de cadastro do CLIENTE
 DELIMITER //
 CREATE TRIGGER Tr_ClienteDataCadastro BEFORE INSERT ON CLIENTE
@@ -153,12 +177,12 @@ DROP TRIGGER Tr_ClienteDataCadastro;
 -- Insere o preço de aluguel do MODELO, baseado em seu preço comercial
 CREATE TRIGGER Tr_UpdateAluguel BEFORE INSERT ON MODELO -- no Insert
 	FOR EACH ROW 
-		set new.PrecoAluguel = 0.002 * new.PrecoModelo;
+		set new.PrecoAluguel = 0.0021 * new.PrecoModelo;
 DROP TRIGGER Tr_UpdateAluguel;
 
 CREATE TRIGGER Tr_InsereAluguel BEFORE UPDATE ON MODELO -- no Update
 	FOR EACH ROW 
-		set new.PrecoAluguel = 0.002 * new.PrecoModelo;
+		set new.PrecoAluguel = 0.0021 * new.PrecoModelo;
 DROP TRIGGER Tr_InsereAluguel;
 
 -- Insere DataInicio e DataFim na tabela GARAGEM quando há locação de um modelo (DataLocacao, DataRetorno)
@@ -167,15 +191,23 @@ CREATE TRIGGER Tr_GaragemLocacao AFTER INSERT ON LOCACAO
 		update garagem set StatusId = 1, DataInicio = new.DataLocacao, DataFim = new.DataRetorno where GaragemId = new.GaragemId;
 DROP TRIGGER Tr_GaragemLocacao;
 
-
 -- Calcula o valor total a pagar Valor = PrecoAluguel * (DataRetorno - DataLocacao)
 CREATE TRIGGER Tr_Pagamento AFTER INSERT ON LOCACAO
 	FOR EACH ROW
 		call ValorTotalAluguel(new.LocacaoId, new.ClienteId, new.GaragemId);
 DROP TRIGGER Tr_Pagamento;
 
---##### /GATILHOS/ #####--
+-- Registra data de atualização do MODELO
+CREATE TRIGGER Tr_UltimoUpdateModelo1 BEFORE INSERT ON MODELO
+	FOR EACH ROW
+		set new.UltimoUpdate = current_timestamp();
+DROP TRIGGER Tr_UltimoUpdateModelo1;
+CREATE TRIGGER Tr_UltimoUpdateModelo2 BEFORE UPDATE ON MODELO
+	FOR EACH ROW
+		set new.UltimoUpdate = current_timestamp();
+DROP TRIGGER Tr_UltimoUpdateModelo2;
 
+--##### /GATILHOS/ #####--
 
 
 --##### TESTES #####--
