9706eb32ab3329331d0ffdb574b8ce167d4d5b92
Almeida <williamgabrielg@gmail.com>
2022-03-17 19:29:01 -0300

    dados para teste e alguns gatilhos

--- sql/Z. teste.sql
+++ sql/Z. teste.sql
@@ -10,18 +10,6 @@ INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (2, '
 -- INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (8, 'DF', '72210-070', 'Brasília', 'Ceilândia Norte (Ceilândia)', 'Quadra QNM 6 Conjunto J');
 -- INSERT INTO ENDERECO(EnderecoId, Uf, Cep, Cidade, Bairro, Endereco) VALUES (9, 'DF', '70002-901', 'Brasília', 'Asa Norte', 'SBN Quadra 1 Bloco A 19º Andar');
 -----------
--- INSERT INTO CLIENTE(ClienteId, Nome, DataNasc, Email, Ativo, EnderecoId) VALUES ('027.994.401-26', 'Bruno Iago Guilherme Pires', '1985-01-15', 'bruno-pires94@zigotto.com.br', '1', 3);
--- INSERT INTO CLIENTE(ClienteId, Nome, DataNasc, Email, Ativo, EnderecoId) VALUES ('750.192.711-10', 'Rosa Clarice da Rocha', '1985-01-15', 'rosaclaricedarocha@ugeda.com.br', '1', 4);
--- INSERT INTO CLIENTE(ClienteId, Nome, DataNasc, Email, Ativo, EnderecoId) VALUES ('195.762.441-88', 'João Emanuel da Paz', '1985-01-15', 'joao-dapaz79@gmai.com', '1', 5);
--- INSERT INTO CLIENTE(ClienteId, Nome, DataNasc, Email, Ativo, EnderecoId) VALUES ('251.189.591-95', 'Cauã Luiz Yago Sales', '1985-01-15', 'caua.luiz.sales@megamega.com.br', '1', 6);
--- INSERT INTO CLIENTE(ClienteId, Nome, DataNasc, Email, Ativo, EnderecoId) VALUES ('043.566.591-04', 'Alexandre Felipe Márcio Peixoto', '1985-01-15', 'alexandre-peixoto78@mktec.com.br', '1', 7);
--- INSERT INTO CLIENTE(ClienteId, Nome, DataNasc, Email, Ativo, EnderecoId) VALUES ('613.501.071-90', 'Bárbara Eloá Pietra Rezende', '1985-01-15', 'barbara-rezende97@redhouse.com.br', '1', 8);
--- INSERT INTO CLIENTE(ClienteId, Nome, DataNasc, Email, Ativo, EnderecoId) VALUES ('226.114.011-87', 'Rodrigo Igor Alves', '1985-01-15', 'rodrigo_alves@sygma.com.br', '1', 9);
-
-
--- Clientes do DF
-
---
 
 -----------
 INSERT INTO CATEGORIA(CategoriaId, Categoria, Lugares, MalaPequena, MalaGrande, Carga, TipoDirecao, CambioAT, Extra) VALUES
@@ -84,37 +72,54 @@ INSERT INTO MODELO(ModeloId, Modelo, Fabricante, Direcao, Cambio, Ano, PrecoMode
 	(22, 'Ecosport 1.5 SE', 'Ford', 'Hidráulica', 'Manual', 2021, 80069.00, 'S0'),
 	(23, 'Ecosport 2.0 Storm 4WD (Aut)', 'Ford', 'Hidráulica', 'Automático', 2021, 106485.00, 'S0t');
 -----------
-INSERT INTO LOCADORA(LocadoraId, Locadora, EnderecoId) VALUES (1, 'Dopler Noroeste', 1);
-INSERT INTO LOCADORA(LocadoraId, Locadora, EnderecoId) VALUES (2, 'Dopler Asa Norte', 2);
+INSERT INTO LOCADORA(LocadoraId, Locadora, EnderecoId) VALUES
+	(1, 'Dopler Noroeste', 1),
+	(2, 'Dopler Asa Norte', 2);
 -----------
-INSERT INTO STATUS(StatusId, Status) VALUES(0, 'Alugado');
-INSERT INTO STATUS(StatusId, Status) VALUES(1, 'Disponível');
-INSERT INTO STATUS(StatusId, Status) VALUES(2, 'Reservado');
-INSERT INTO STATUS(StatusId, Status) VALUES(3, 'Manutenção');
+INSERT INTO STATUS(StatusId, Status) VALUES
+	(0, 'Alugado'),
+	(1, 'Disponível'),
+	(2, 'Reservado'),
+	(3, 'Manutenção');
 -----------
-INSERT INTO GARAGEM(GaragemId, ModeloId, LocadoraId) VALUES(1, 1, 1);
-INSERT INTO GARAGEM(GaragemId, ModeloId, LocadoraId) VALUES(2, 2, 1);
-INSERT INTO GARAGEM(GaragemId, ModeloId, LocadoraId) VALUES(3, 3, 1);
-INSERT INTO GARAGEM(GaragemId, ModeloId, LocadoraId) VALUES(4, 1, 2);
-INSERT INTO GARAGEM(GaragemId, ModeloId, LocadoraId) VALUES(5, 2, 2);
-INSERT INTO GARAGEM(GaragemId, ModeloId, LocadoraId) VALUES(6, 3, 2);
+INSERT INTO GARAGEM(ModeloId, LocadoraId) VALUES
+	(3, 1),
+	(4, 1),
+	(8, 1),
+	(9, 1),
+	(10, 1),
+	(14, 1),
+	(15, 1),
+	(18, 1),
+	(20, 1),
+	(21, 1),
+	(22, 1),
+	(23, 1);
+INSERT INTO GARAGEM(ModeloId, LocadoraId) VALUES
+	(1, 2),
+	(2, 2),
+	(5, 2),
+	(6, 2),
+	(7, 2),
+	(11, 2),
+	(12, 2),
+	(13, 2),
+	(16, 2),
+	(17, 2),
+	(18, 2),
+	(19, 2);
+	(20, 2);
+	(21, 2);
+	(22, 2);
 -----------
-INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('027.994.401-26', 1, '2022-03-15', '2022-03-20');
-INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('750.192.711-10', 2, '2022-03-24', '2022-04-02');
-INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('195.762.441-88', 3, '2022-04-01', '2022-04-10');
-INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('251.189.591-95', 4, '2022-03-16', '2022-05-14');
 
-INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('043.566.591-04', 5, '2022-03-16', '2022-05-14');
------------
-INSERT INTO PAGAMENTO(PagamentoId, ClienteId, LocacaoId, Valor) VALUES;
 --##### /DADOS PARA TESTE/ #####--
 
 
 --##### PROCEDIMENTOS #####--
-
 -- Cadastra CLIENTE
 DELIMITER //
-CREATE PROCEDURE CadastraCliente(xNome varchar(40), xClienteId varchar(14), xRg varchar(12), xDataNasc varchar(10), xSexo varchar(10), xEmail varchar(40), xSenha varchar(40), xCep varchar(9), xEndereco varchar(40), xBairro varchar(50), xCidade varchar(30), xUf char(2), xTelefone varchar(20))
+CREATE PROCEDURE CadastraCliente(xNome varchar(40), xClienteId varchar(14), xRg varchar(12), xDataNasc varchar(10), xSexo varchar(10), xEmail varchar(60), xSenha varchar(40), xCep varchar(9), xEndereco varchar(40), xBairro varchar(60), xCidade varchar(30), xUf char(2), xTelefone varchar(20))
 	BEGIN
 		-- converte DataNasc e Sexo para os formatos padrão
 		declare xEnderecoId smallint;
@@ -185,10 +190,10 @@ CREATE TRIGGER Tr_InsereAluguel BEFORE UPDATE ON MODELO -- no Update
 		set new.PrecoAluguel = 0.0021 * new.PrecoModelo;
 DROP TRIGGER Tr_InsereAluguel;
 
--- Insere DataInicio e DataFim na tabela GARAGEM quando há locação de um modelo (DataLocacao, DataRetorno)
-CREATE TRIGGER Tr_GaragemLocacao AFTER INSERT ON LOCACAO
+-- Define Status de aluguel em GARAGEM quando há LOCAÇÃO de um modelo, e insere as datas de alguel na tabela (DataLocacao, DataRetorno)
+CREATE TRIGGER Tr_Locacao AFTER INSERT ON LOCACAO
 	FOR EACH ROW
-		update garagem set StatusId = 1, DataInicio = new.DataLocacao, DataFim = new.DataRetorno where GaragemId = new.GaragemId;
+		update garagem set StatusId = 0, DataInicio = new.DataLocacao, DataFim = new.DataRetorno where GaragemId = new.GaragemId;
 DROP TRIGGER Tr_GaragemLocacao;
 
 -- Calcula o valor total a pagar Valor = PrecoAluguel * (DataRetorno - DataLocacao)
@@ -207,6 +212,26 @@ CREATE TRIGGER Tr_UltimoUpdateModelo2 BEFORE UPDATE ON MODELO
 		set new.UltimoUpdate = current_timestamp();
 DROP TRIGGER Tr_UltimoUpdateModelo2;
 
+-- Registra data de atualização em GARAGEM
+CREATE TRIGGER Tr_UltimoUpdateGaragem1 BEFORE INSERT ON GARAGEM
+	FOR EACH ROW
+		set new.UltimoUpdate = current_timestamp();
+DROP TRIGGER Tr_UltimoUpdateGaragem1;
+CREATE TRIGGER Tr_UltimoUpdateGaragem2 BEFORE UPDATE ON GARAGEM
+	FOR EACH ROW
+		set new.UltimoUpdate = current_timestamp();
+DROP TRIGGER Tr_UltimoUpdateGaragem2;
+
+-- Registra data de atualização em LOCACAO
+CREATE TRIGGER Tr_UltimoUpdateLocacao1 BEFORE INSERT ON LOCACAO
+	FOR EACH ROW
+		set new.UltimoUpdate = current_timestamp();
+DROP TRIGGER Tr_UltimoUpdateLocacao1;
+CREATE TRIGGER Tr_UltimoUpdateLocacao2 BEFORE UPDATE ON LOCACAO
+	FOR EACH ROW
+		set new.UltimoUpdate = current_timestamp();
+DROP TRIGGER Tr_UltimoUpdateLocacao2;
+
 --##### /GATILHOS/ #####--
 
 
@@ -220,4 +245,43 @@ SELECT * FROM GARAGEM;
 SELECT * FROM LOCACAO;
 SELECT * FROM STATUS;
 SELECT * FROM PAGAMENTO;
+
+-----------
+CALL CadastraCliente('Fernando Severino Aragão','755.244.601-37','46.731.548-6','06/01/1961','Masculino','fernandoseverinoaragao@gripoantonin.com','IIezCNXzt3','72594-001','Quadra QBR 7 Bloco A','Residencial Santos Dumont (Santa Maria)','Brasília','DF','(61) 99383-3185');
+CALL CadastraCliente('Marcelo Pietro da Rocha','104.127.001-15','10.577.573-3','02/02/1978','Masculino','marcelopietrodarocha@sygma.com.br','yRGNSqybuM','70658-613','Quadra SHCES Quadra 1601 Bloco C','Cruzeiro Novo','Brasília','DF','(61) 99102-4511');
+CALL CadastraCliente('Kaique Benedito Corte Real','310.678.151-39','13.997.823-9','01/03/2003','Masculino','kaiquebeneditocortereal@bluewash.com.br','oiIpoIeDPh','72579-370','Chácara Chácara 37','Condomínio Chácaras Ana Maria (Santa Maria)','Brasília','DF','(61) 99468-8152');
+CALL CadastraCliente('Stella Francisca Luzia Aparício','085.242.381-00','45.375.454-5','10/01/1955','Feminino','stella-aparicio91@technicolor.com','s4qfwD8x5d','72311-407','Quadra QN 503 Conjunto 7','Samambaia Sul (Samambaia)','Brasília','DF','(61) 98187-9419');
+CALL CadastraCliente('Joaquim Gustavo Pires','714.841.161-00','49.194.944-3','09/03/1958','Masculino','joaquim.gustavo.pires@zootecnista.com.br','xUrwR4rCBH','70730-714','Quadra SHCGN 703 Bloco N','Asa Norte','Brasília','DF','(61) 99658-1315');
+CALL CadastraCliente('César Ian Jorge Silveira','229.020.111-13','31.918.078-5','10/03/1981','Masculino','cesar-silveira87@sebrace.com.br','CJKapFe8Sd','72231-222','Quadra QNP 12 Conjunto V','Ceilândia Sul (Ceilândia)','Brasília','DF','(61) 98650-1245');
+CALL CadastraCliente('Ayla Sônia Gomes','516.001.511-67','12.142.303-7','04/03/2003','Feminino','ayla_sonia_gomes@semco.com.br','cN1oRA70Gj','72500-420','Quadra QR 100 Conjunto N','Santa Maria','Brasília','DF','(61) 99299-0453');
+CALL CadastraCliente('Agatha Beatriz Isabelly da Rosa','226.484.561-98','21.322.769-1','11/03/1988','Feminino','agatha_beatriz_darosa@greenschool.com.br','ruLLDg7Uyi','71593-637','Quadra 378 Conjunto R','Del Lago II (Itapoã)','Brasília','DF','(61) 98573-3435');
+CALL CadastraCliente('Simone Carla Daniela Barbosa','523.100.131-70','35.955.552-4','18/02/1952','Feminino','simone_barbosa@coreval.com.br','OEWQqTzPlK','71691-075','Quadra CMA 04','Centro (São Sebastião)','Brasília','DF','(61) 99509-3364');
+CALL CadastraCliente('José Rodrigo Araújo','948.321.941-86','35.204.143-2','01/03/2000','Masculino','joserodrigoaraujo@aspxsistemas.com.br','H5FLhRTguK','71680-370','Condomínio Quintas do Sol','Setor Habitacional Jardim Botânico (Lago Sul)','Brasília','DF','(61) 99128-5771');
+CALL CadastraCliente('Raquel Alessandra Luna Rodrigues','540.443.101-65','11.673.878-9','17/02/1982','Feminino','raquel_alessandra_rodrigues@flex-erp.com','0c9XfoPEGS','72410-202','Quadra Quadra 3 Conjunto B','Setor Sul (Gama)','Brasília','DF','(61) 99223-3496');
+CALL CadastraCliente('Diogo Benedito Santos','239.540.891-30','46.879.236-3','06/01/1989','Masculino','diogo.benedito.santos@negleribeiro.com','BTeQNXffuZ','71300-000','Vila Estrutural','Vila Estrutural','Brasília','DF','(61) 98606-6320');
+CALL CadastraCliente('André Cauã Rodrigo Galvão','234.782.191-16','49.153.108-4','11/03/1966','Masculino','andrecauagalvao@zipmail.com','iuOKsYR7uu','71805-132','Quadra QN 1 Conjunto 32','Riacho Fundo I','Brasília','DF','(61) 99912-8084');
+CALL CadastraCliente('Maitê Stella Oliveira','398.075.771-44','47.658.885-6','05/02/1979','Feminino','maitestellaoliveira@br.atlascopco.com','iH0rCekWbb','72310-214','Conjunto SMSE Conjunto 14','Samambaia Sul (Samambaia)','Brasília','DF','(61) 99551-4143');
+CALL CadastraCliente('Marcos Vinicius Murilo Iago da Paz','276.811.351-46','33.716.198-7','21/01/1982','Masculino','marcos.vinicius.dapaz@platinium.com.br','ffI2E1EUmu','72308-203','Quadra QN 314 Conjunto 3','Samambaia Sul (Samambaia)','Brasília','DF','(61) 98496-4545');
+CALL CadastraCliente('Rodrigo Marcelo Isaac Rezende','942.845.421-41','44.537.170-5','06/03/1962','Masculino','rodrigo_rezende@parkhotel.com.br','oR4TIceH1s','70374-050','Quadra SQS 111 Bloco E','Asa Sul','Brasília','DF','(61) 99891-3408');
+CALL CadastraCliente('Sebastiana Isabelly Antônia Silva','222.740.441-80','46.157.762-8','06/02/1951','Feminino','sebastianaisabelly@soulcomunic.com.br','mqSdkcOzub','70299-130','Quadra SQS 416 Bloco M','Asa Sul','Brasília','DF','(61) 99816-6644');
+CALL CadastraCliente('Priscila Elisa Fernanda Moura','400.255.401-57','10.843.242-7','14/01/1964','Feminino','priscila-moura81@agaxtur.com.br','xXCaX3p08s','71660-260','Quadra SHIS QI 25 Conjunto 6','Setor de Habitações Individuais Sul','Brasília','DF','(61) 99834-9866');
+CALL CadastraCliente('Antônia Gabriela das Neves','913.693.301-54','31.778.507-2','24/02/1976','Feminino','antonia-dasneves82@gmail.com.br','TS8wztRx3q','73041-710','Quadra 13 AR 04','Sobradinho','Brasília','DF','(61) 98566-8555');
+CALL CadastraCliente('Camila Renata Lavínia Jesus','287.926.001-90','50.027.861-1','14/03/1995','Feminino','camilarenatajesus@lnaa.com.br','AjtHiw80kt','72317-522','Quadra QN 523 Conjunto B','Samambaia Sul (Samambaia)','Brasília','DF','(61) 98424-6835');
+CALL CadastraCliente('Kauê Noah Márcio Teixeira','887.644.421-10','22.049.749-7','02/03/1965','Masculino','kaue-teixeira79@recnev.com.br','2Hxfa0PQo9','72725-403','Quadra Quadra 4 Conjunto C','Veredas (Brazlândia)','Brasília','DF','(61) 98640-7244');
+CALL CadastraCliente('Elias Manuel Severino Barros','799.612.211-33','50.415.522-2','06/03/1980','Masculino','elias.manuel.barros@inforgel.com','zazNZOtLKq','72265-730','Quadra SMC Quadra 7','Setor de Materiais de Construção (Ceilândia)','Brasília','DF','(61) 98281-0892');
+CALL CadastraCliente('Renato Joaquim Nogueira','017.739.411-00','42.741.549-4','04/03/1959','Masculino','renato_joaquim_nogueira@jeffersonbarroso.com.br','SR6CEpkF0X','71570-813','Quadra Quadra 8 Conjunto M','Paranoá','Brasília','DF','(61) 99141-2836');
+CALL CadastraCliente('Fernando Pedro Henrique Viana','010.628.011-21','11.109.333-8','12/03/1980','Masculino','fernando-viana94@gruporeis.net','NEImFP6Jb7','72579-110','Chácara Chácara 11','Condomínio Chácaras Ana Maria (Santa Maria)','Brasília','DF','(61) 98595-8433');
+CALL CadastraCliente('Daniela Evelyn Antonella Brito','953.977.851-48','23.462.085-7','18/02/1945','Feminino','daniela_evelyn_brito@gmail.com.br','kN4giXlf56','70684-030','Quadra SQNW 304 Bloco E','Setor Noroeste','Brasília','DF','(61) 99914-4915');
+CALL CadastraCliente('Thales Cauê Renan Fernandes','514.164.011-64','50.625.120-2','23/02/1991','Masculino','thales.caue.fernandes@cancaonova.com','t9U697EfIi','72542-223','Quadra CL 212 Bloco C','Santa Maria','Brasília','DF','(61) 99881-6059');
+CALL CadastraCliente('Antonio Osvaldo Nascimento','786.943.731-06','43.770.143-8','08/01/1994','Masculino','antonio_nascimento@madhause.com.br','2oPBdBKP2z','71736-300','Quadra Quadra 3','Setor de Indústrias Bernardo Sayão (Núcleo Bandeirante)','Brasília','DF','(61) 99316-3714');
+CALL CadastraCliente('Aline Cristiane Moreira','101.362.131-03','12.068.457-3','25/01/2003','Feminino','aline.cristiane.moreira@freitasprior.com.br','akKloOD0OK','70790-147','Quadra SGAN 914 Módulo G','Asa Norte','Brasília','DF','(61) 98582-6132');
+CALL CadastraCliente('Diogo Thales Caldeira','631.849.151-15','30.733.453-3','22/02/1950','Masculino','diogo_thales_caldeira@gruposimoes.com.br','ycTp6DHlZN','73340-511','Quadra Quadra 5 Conjunto 5K','Jardim Roriz (Planaltina)','Brasília','DF','(61) 99406-8220');
+CALL CadastraCliente('Lavínia Lúcia Farias','639.630.271-34','18.994.247-2','04/03/1977','Feminino','lavinialuciafarias@igui.com.br','Nbhpog5T1z','71665-055','Quadra SHIS QL 24 Conjunto 5','Setor de Habitações Individuais Sul','Brasília','DF','(61) 98701-3045');
+-----------
+INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('755.244.601-37', 1, '2022-03-15', '2022-03-20');
+INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('104.127.001-15', 2, '2022-03-24', '2022-04-02');
+INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('310.678.151-39', 3, '2022-04-01', '2022-04-10');
+INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('085.242.381-00', 4, '2022-03-16', '2022-05-14');
+INSERT INTO LOCACAO(ClienteId, GaragemId, DataLocacao, DataRetorno) VALUES('639.630.271-34', 1, '2022-04-16', '2022-04-22');
+-----------
 --##### /TESTES/ #####--
